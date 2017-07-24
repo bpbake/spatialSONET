ee2192eccb12656310768f7a7a0c89d4faa3d327
Brittany <bake0573@umn.edu>
2017-07-24 10:05:13 -0500

    added new analyze_results script
    Includes plot_results and reformat_results functions.  Script is
    incomplete

---
+++ analyze_results.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 09:46:52 2017
+
+@author: Brittany
+"""
+def plot_results(N,p,i, data_dir='matrices/'):
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+	try:
+	   import cPickle as pickle
+	except:
+	   import pickle
+    
+    result_filename = "{0}Results_W_N{1}_p{2}_slower{3}.pickle".format(data_dir,N,p,i) 
+    with open(result_filename, "rb") as rf:
+       results = pickle.load(rf)
+
+    print('Matrix {0}'.format(i))
+    for k,v in sorted(results.items()):
+        if not isinstance(v,np.ndarray):
+            print(k+":{0}".format(v))
+    print("\n")
+
+
+    matplotlib.rcParams.update({'font.size': 20})
+    plt.rc('xtick', labelsize=15)
+    plt.rc('ytick', labelsize=15)
+
+    # #plot the results of the simulation
+    plt.figure(figsize=(20,7))
+    # #subplot(122)
+    # #plot(simulation_statemon.t/ms, simulation_statemon.v[0])
+    # #xlabel('Time (ms)')
+    # #ylabel('v')
+    
+    mintime=500
+    maxtime=3500
+    plt.subplot(211)
+
+    plt.suptitle('Matrix {0}'.format(i))    
+
+    inds = np.logical_and(results['spikemon times']>mintime, results['spikemon times'] < maxtime)
+    plt.plot(results['spikemon times'][inds],results['spikemon indices'][inds], '.k', markersize=1)
+    
+    #axis([mintime, maxtime, 1, N])
+    #xlabel('Time (ms)')
+    plt.xticks([])
+    plt.ylabel('Neuron index')
+    plt.tight_layout()
+    
+    plt.subplot(212)
+    ind1=np.min(np.where(results['PRM time']>mintime))
+    ind2=np.max(np.where(results['PRM time']<maxtime))
+    plt.plot(results['PRM time'][ind1:ind2],results['PRM rate'][ind1:ind2])
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Population rate (Hz)')
+    #axis([1500, 2000, 0, ceil(max(results['PRM rate'])/100)*100])
+    plt.tight_layout()
+
+
+def reformat_results(N,p,i, data_dir='matrices/'):
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+	try:
+	   import cPickle as pickle
+	except:
+	   import pickle
+
+    
+    result_filename = "{0}Results_W_N{1}_p{2}_slower{3}.pickle".format(data_dir,N,p,i) 
+    with open(result_filename, "rb") as rf:
+       results = pickle.load(rf)
+
+    print('Matrix {0}'.format(i))
+    for k,v in sorted(results.items()):
+        if not isinstance(v,np.ndarray):
+            print(k+":{0}".format(v))
+    print("\n")
+
