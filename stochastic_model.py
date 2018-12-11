62463a1ea0892f595ae297214660320cba6eedcc
Brittany <bake0573@umn.edu>
2018-12-11 11:52:24 -0600

    stochastic_model.py runs. needs tweaking

--- stochastic_model.py
+++ stochastic_model.py
@@ -11,9 +11,10 @@ def stochastic_model(W, N, tmax):
 	import numpy as np	
 
 	t = 0
-	dt = 0.01 #ms
+	coupling_strength = 0.01
+	ext_input = 0.001 ## constant rate of external input
 	death = 1 ## constant "death rate" (rate at which active neurons become inactive)
-	birth = np.sum(W,axis=1) #
+	nodes = np.arange(1,N+1).flatten()
 
 	times = []
 	neurons = []
@@ -22,18 +23,22 @@ def stochastic_model(W, N, tmax):
 
 	while t<tmax:
 		## calculate the current total rate
+		birth = ext_input + np.squeeze(np.asarray(np.multiply(coupling_strength, np.matmul(W,active_N)))) #
+		# print("birth vector shape: {0}".format(birth.shape))
 		rate_vector = death*active_N + np.multiply((1-active_N), birth)
 		total_rate =  np.sum(rate_vector)
 
 		## generated an exponential random variable 
-		delta_t = np.random.exponential(1/total_rate)
-		t += delta_t
+		x_t = np.random.exponential(1/total_rate)
+		t += x_t
 		times.append(t) ## update the list of times
 
-		## determine which neuron flipped
-		X_flip = np.random.uniform(0,1) 
-		N_flip = #use X_flip to define which neuron flipped
+		## determine which neuron flipped_rate
+		probabilities = np.true_divide(rate_vector, total_rate).flatten()
+		N_flip = int(np.random.choice(nodes, 1, p=probabilities))
 
 		## update neurons list and active neuron vector
-		neurons.appennd(N_flip)
-		active_N[N_flip] = 1-active_N[N_flip]
+		neurons.append(N_flip)
+		active_N[N_flip-1] = 1-active_N[N_flip-1]
+
+	return(times,neurons)
